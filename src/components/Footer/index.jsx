f070b53d63cc3a5fc48485d81de48014d5b876f0
Rafael Moura <rafaelfelipe.peixoto@gmail.com>
2022-03-31 19:08:52 -0300

    Teste

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,12 +7,7 @@ function Footer() {
       <h4>Weather App</h4>
       <p>Desenvolvido por Rafael Moura</p>
       <div className="social--medias">
-        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/rafael-moura-dev/">
-          <i className="bi bi-linkedin" ></i>
-        </a>
-        <a target="_blank" rel="noreferrer" href="https://github.com/Rafa-Moura">
-          <i className="bi bi-github"></i>
-        </a>
+ 
       </div>
     </footer>
   );
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,33 @@
 import Logo from "../../assets/img/logo.png";
 
-import './styles.css'
+import "./styles.css";
 
 function Navbar() {
   return (
     <header>
-      <div className="logo--navbar">
-        <a href="/">
-          <img src={Logo} alt="" />
-          Weather App
-        </a>
+      <div className="header--navbar">
+        <div className="logo">
+          <a href="/">
+            <img src={Logo} alt="" />
+            <span>Weather App</span>
+          </a>
+        </div>
+        <div className="social--medias">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/rafael-moura-dev/"
+          >
+            <i className="bi bi-linkedin"></i>
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Rafa-Moura"
+          >
+            <i className="bi bi-github"></i>
+          </a>
+        </div>
       </div>
     </header>
   );
